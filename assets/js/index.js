323ac4ade3b13a8122f2d957db86d27834afedcf
Josh <josh@Joshs-iMac.local>
2019-04-20 17:40:52 -0700

    Update instagram, bio, footer style

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,58 @@
 (function() {
+
+  function createMediaElement(imgUrl, link) {
+    let postElem = document.createElement('div');
+    postElem.classList.add('ig-feed__post');
+
+    let linkElem = document.createElement('a');
+    let imageElem = document.createElement('img');
+    imageElem.classList.add('ig-feed__media')
+
+    imageElem.src = imgUrl;
+    linkElem.href = link;
+
+    linkElem.appendChild(imageElem);
+    postElem.appendChild(linkElem);
+
+    return postElem;
+  }
+
   window.addEventListener('load', function() {
-num_posts: 8
     const ACCESS_TOKEN = "1447508261.1677ed0.6ebb3172543d49c8a29fca4b7415fba7"
-    const COUNT = 8;
+    const COUNT = 9;
 
     const URL = 'https://api.instagram.com/v1/users/self';
     
     let mediaRequest = URL + '/media/recent?access_token=' + ACCESS_TOKEN + '&count=' + COUNT;
     let userRequest = URL + '/?access_token=' + ACCESS_TOKEN;
 
-    let profile = document.getElementById('ig-profile');
-    let feed = document.getElementById('ig-feed');
+    let feed = document.getElementById('js-ig-feed');
     
     fetch(mediaRequest)
       .then(data => data.json())
       .then((results) => {
         results.data.forEach((post) => {
-          let postElem = document.createElement('a');
-          let imageElem = document.createElement('img');
-
-          imageElem.src = post.images.standard_resolution.url;
-          postElem.href = post.link;
-
-          postElem.appendChild(imageElem);
-          feed.appendChild(postElem);
+          feed.appendChild(createMediaElement(
+            post.images.standard_resolution.url,
+            post.link
+          ));
         });
       });
 
     fetch(userRequest)
       .then(data => data.json())
       .then((results) => {
-        let handleElem = document.getElementById('ig-handle');
-        let pictureElem = document.getElementById('ig-profilepic');
-        let followersElem = document.getElementById('ig-followers');
-        let postsElem = document.getElementById('ig-posts');
+        let handleElem = document.getElementById('js-ig-handle');
+        let pictureElem = document.getElementById('js-ig-pic');
+        let statsElem = document.getElementById('js-ig-stats');
 
         handleElem.innerHTML = '@' + results.data.username;
         pictureElem.src = results.data.profile_picture;
-        followersElem.innerHTML = results.data.counts.followed_by;
-        postsElem.innerHTML = results.data.counts.media;
+
+        let followers = results.data.counts.followed_by;
+        let posts = results.data.counts.media;
+        let statsString = `${followers} Followers - ${posts} Posts`;
+        statsElem.innerHTML = statsString;
       });
   });
 })();
